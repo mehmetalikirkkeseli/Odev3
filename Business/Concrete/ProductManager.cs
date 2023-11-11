892d4faf3cf27a104fd6e9e8f7f048a5c41417b7
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 17:05:04 +0300

    commit17

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,19 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
         public List<Product> GetAll()
         {
             //İş kodları
+            //Yetkisi var mı?
+
+            return _productDal.GetAll();
         }
     }
 }
