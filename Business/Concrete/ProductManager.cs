1e071e0c332239026d18d03c2712879bada32671
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 16:56:29 +0300

    commit16

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public List<Product> GetAll()
+        {
+            //İş kodları
+        }
+    }
+}
