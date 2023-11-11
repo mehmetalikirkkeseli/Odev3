3ed2f0073c22acfa06bf500fd8b0eeea7126eac8
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 19:10:09 +0300

    commit4

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,10 @@ namespace DataAccess.Abstract
 {
     public interface IEntityRepository<T>
     {
-        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllCategory(int categoryId);
     }
 }
