42aff727cf41083e357fa7cb0a9b2776faf7881c
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 19:28:13 +0300

    commit6

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal:IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
         
     }
