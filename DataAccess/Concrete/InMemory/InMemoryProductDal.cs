a4ab450bd5035fbae70e8e89f61c214a5ad23f5a
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 13:05:09 +0300

    commit8

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -10,6 +10,20 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server,Postgres,MongoDb benzeri
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+
+            };
+           
+        }
         public void Add(Product product)
         {
             throw new NotImplementedException();
