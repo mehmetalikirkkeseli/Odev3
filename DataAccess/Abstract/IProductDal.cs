7bfa04bc49bafda2030f094d5d56f4d35b0df64b
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 12:34:56 +0300

    commit6

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal
     {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
 
     }
 }
