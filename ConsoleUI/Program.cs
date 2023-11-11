f223c5daace47c739b25380f3485f456500f87b2
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-11 17:16:13 +0300

    commit18

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ConsoleUI
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
         }
     }
 }
